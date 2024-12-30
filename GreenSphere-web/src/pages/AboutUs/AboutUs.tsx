476084da9d610420d90ba29fa5d75ea095a30ce7
Guilherme Miyata <miyatagui120600@gmail.com>
2024-12-30 19:15:21 -0300

    feat: adapted the app to a mock API Adapted the splide, product page and plant page to receive data from the mock API Created a "useFetchPlant" hook to avoid code repetition. Added a loader component that remains active while the API information is still being requested.

--- GreenSphere-web/src/pages/AboutUs/AboutUs.tsx
+++ GreenSphere-web/src/pages/AboutUs/AboutUs.tsx
@@ -17,7 +17,7 @@ const AboutUs = () => {
 
         <FlipCard email="miyata120600@gmail.com" devImg={Miyata} icon={FrontEnd} developed="Front End" name="Guilherme Miyata" education="Control and Automation Engineering" company="IFSP - Federal Institute of São Paulo" role="Student" age="24" location="São José dos Campos - SP - Brazil" linkedin="https://www.linkedin.com/in/guilherme-miyata-612a71219/" github="https://github.com/g-miyata" />
 
-        <FlipCard email="francisco.maldona.pb@compasso.com.br" devImg={Francisco} icon={BackEnd} developed="Back End" name="Francisco G. Maldonado" education="Systems Analysis and Development" company="IFSP - Federal Institute of São Paulo" role="Student" age="21" location="São Carlos - SP - Brazil" linkedin="www.linkedin.com/in/francisco-guatura-maldonado-290402209" github="https://github.com/FranciscoGMaldonado" />
+        <FlipCard email="francisco.maldona.pb@compasso.com.br" devImg={Francisco} icon={BackEnd} developed="Back End" name="Francisco G. Maldonado" education="Systems Analysis and Development" company="IFSP - Federal Institute of São Paulo" role="Student" age="21" location="São Carlos - SP - Brazil" linkedin="https://www.linkedin.com/in/francisco-guatura-maldonado-290402209/" github="https://github.com/FranciscoGMaldonado" />
 
         <FlipCard email="vinicius.vezono123@gmail.com" devImg={Miyata} icon={BackEnd} developed="Back End" name="Vinicius Vezono" education="Systems Analysis and Development" company="IFSP - Federal Institute of São Paulo" role="Student" age="19" location="Barretos - SP - Brazil" linkedin="https://www.linkedin.com/in/vinicius-vezono/" github="https://github.com/ViniciusVezono" />
       </div>
