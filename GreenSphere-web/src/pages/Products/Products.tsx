2ce458c9d6b8404792ebc69033ff935b7f58a923
Guilherme Miyata <miyatagui120600@gmail.com>
2025-01-03 01:23:24 -0300

    feat: added search bar and filtering functionality with usePlantFilters hook. Implemented a search bar to filter plants by name. Added radio buttons to filter plants by type (Indoor, Outdoor, All). Created a new hook, usePlantFilters, to handle filtering logic based on search terms and plant type.

--- GreenSphere-web/src/pages/Products/Products.tsx
+++ GreenSphere-web/src/pages/Products/Products.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useFetchPlants from '../../hooks/API/useFetchPlants';
+import usePlantFilters from '../../hooks/usePlantFilter';
 import Loader from '../../components/Loader/Loader';
 import Label from '../../components/Label/Label';
 import Button from '../../components/Button/Button';
@@ -9,22 +10,35 @@ import Titles from '../../components/Titles/Titles';
 
 const Products: React.FC = () => {
   const { data, loading, error } = useFetchPlants();
+  const { filteredPlants, setSearchTerm, setFilter } = usePlantFilters(data || []);
 
-  if (loading)
-    return (
-      <p>
-        <Loader />
-      </p>
-    );
+  if (loading) return <Loader />;
   if (error) return <p>Erro: {error}</p>;
 
   return (
     <div className={style.container}>
       <Titles titleText="Our Products" />
+      <div className={style.filterContainer}>
+        <input type="text" placeholder="Search by name..." onChange={(e) => setSearchTerm(e.target.value)} className={style.searchBar} />
+        <div className={style.filterGroup}>
+          <label htmlFor="filter-all">
+            <input id="filter-all" type="radio" name="filter" value="all" defaultChecked onChange={() => setFilter('all')} />
+            All
+          </label>
+          <label htmlFor="filter-indoor">
+            <input id="filter-indoor" type="radio" name="filter" value="Indoor" onChange={() => setFilter('Indoor')} />
+            Indoor
+          </label>
+          <label htmlFor="filter-outdoor">
+            <input id="filter-outdoor" type="radio" name="filter" value="Outdoor" onChange={() => setFilter('Outdoor')} />
+            Outdoor
+          </label>
+        </div>
+      </div>
       <div className={style.contentContainer}>
         <ul className={style.list}>
-          {data && data.length > 0 ? (
-            data.map((plant) => (
+          {filteredPlants.length > 0 ? (
+            filteredPlants.map((plant) => (
               <li key={plant.id}>
                 <div className={style.cardBody}>
                   <div className={style.imgContainer}>
