c7ae2fc646f1de39d681c04c89093c545cf8c079
Guilherme Miyata <miyatagui120600@gmail.com>
2025-01-23 12:08:58 -0300

    Docs: Added Dockerfiles

--- GreenSphere-web/src/components/Label/Label.tsx
+++ GreenSphere-web/src/components/Label/Label.tsx
@@ -2,6 +2,7 @@ import style from './Label.module.css';
 import { LabelProps } from './Label.types';
 
 const Label = ({ text }: LabelProps) => {
+  if (!text) return;
   const content = text.toLocaleLowerCase().trim();
   return <div className={style.label}>{content}</div>;
 };
